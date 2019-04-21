bb542ed207e43b6097b7845654e6215d19fe42b1
unsubstantiatedpod <41484967+unsubstantiatedpod@users.noreply.github.com>
2019-04-21 14:21:08 +0800

    Update remilbio.js

--- draft new/remilbio.js
+++ draft new/remilbio.js
@@ -9,5 +9,7 @@ quotes[1]='bio2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean 
 
 quotes[2]='bio3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.'
 
+quotes[3]='<img src="unsubstantiatedpod.github.io/images/idiot.jpg" alt="idiot" width="200" height="200">'
+
 var whichquote=Math.floor(Math.random()*(quotes.length))
 document.write(quotes[whichquote])
