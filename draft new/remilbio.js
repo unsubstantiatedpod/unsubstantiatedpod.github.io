2f8973c618c61936fc69c651048ca9b8c53a09f2
unsubstantiatedpod <41484967+unsubstantiatedpod@users.noreply.github.com>
2019-04-21 15:09:29 +0800

    Update remilbio.js

--- draft new/remilbio.js
+++ draft new/remilbio.js
@@ -3,16 +3,16 @@ var quotes=new Array()
 
 //change the quotes if desired. Add/ delete additional quotes as desired.
 
-quotes[0]='bio1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa. </br> <a href="www.twitter.com/ihaveargyles">twitter</a>/
+quotes[0]='bio1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa. </br> <a href="www.twitter.com/ihaveargyles">twitter</a>|
 <a href="www.twitter.com/ihaveargyles">instagram</a>'
 
-quotes[1]='bio2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.</br> <a href="www.twitter.com/ihaveargyles">twitter</a>/
+quotes[1]='bio2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.</br> <a href="www.twitter.com/ihaveargyles">twitter</a>|
 <a href="www.twitter.com/ihaveargyles">instagram</a>'
 
-quotes[2]='bio3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa. </br> <a href="www.twitter.com/ihaveargyles">twitter</a>/
+quotes[2]='bio3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa. </br> <a href="www.twitter.com/ihaveargyles">twitter</a>|
 <a href="www.twitter.com/ihaveargyles">instagram</a>'
 
-quotes[3]='<img  class="center" src="https://unsubstantiatedpod.github.io/images/idiot.jpg" alt="idiot" width="200" height="200"> </br> <a href="www.twitter.com/ihaveargyles">twitter</a>/
+quotes[3]='<img  class="center" src="https://unsubstantiatedpod.github.io/images/idiot.jpg" alt="idiot" width="200" height="200"> </br> <a href="www.twitter.com/ihaveargyles">twitter</a>|
 <a href="www.twitter.com/ihaveargyles">instagram</a>'
 
 var whichquote=Math.floor(Math.random()*(quotes.length))
