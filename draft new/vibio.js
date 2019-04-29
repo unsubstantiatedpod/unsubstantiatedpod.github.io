ea8e185eaf817ff1d1437706784332dd7f5a5f4d
unsubstantiatedpod <41484967+unsubstantiatedpod@users.noreply.github.com>
2019-04-29 22:51:32 +0800

    Update vibio.js

--- draft new/vibio.js
+++ draft new/vibio.js
@@ -9,7 +9,19 @@ quotes[1]='bio2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean 
 
 quotes[2]='bio3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.'
 
-quotes[3]='<img class="center" src="https://unsubstantiatedpod.github.io/images/dumbass.jpg" alt="dumbass" width="200" height="200">'
+quotes[3]='bio1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.'
+
+quotes[4]='bio2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.'
+
+quotes[5]='bio3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.'
+
+quotes[6]='bio1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.'
+
+quotes[7]='bio2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.'
+
+quotes[8]='bio3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a volutpat ex. Donec vitae diam accumsan, congue mauris nec, auctor augue. Cras malesuada ut risus quis egestas. In aliquam sollicitudin tellus, vel consectetur massa gravida eu. Fusce massa tellus, pretium vel sagittis ac, semper eget massa.'
+
+quotes[9]='<img class="center" src="https://unsubstantiatedpod.github.io/images/dumbass.jpg" alt="dumbass" width="200" height="200">'
 
 var whichquote=Math.floor(Math.random()*(quotes.length))
 document.write(quotes[whichquote])
