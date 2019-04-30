b0d9311ffb2c51ac1b8f7b019686442d778cad90
unsubstantiatedpod <41484967+unsubstantiatedpod@users.noreply.github.com>
2019-04-30 11:50:03 +0800

    Update status.js

--- draft new/status.js
+++ draft new/status.js
@@ -1,1 +1,1 @@
-document.write("<p><em>"two idiots discover broadcasting"</em><br>a long and winding, unsubstantiated podcast by remil and vi<br><strong>status:[still on hiatus but we have a new website!]</strong></script></p>")
+document.write("<p><em>"two idiots discover broadcasting"</em><br>a long and winding, unsubstantiated podcast by remil and vi<br><strong>status:[still on hiatus but we have a new website!]</strong></p>")
